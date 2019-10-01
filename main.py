456701243793187aee0fada2510b0428b298496b
jose <josloe@gmail.com>
2019-10-01 18:59:07 +0200

    hola

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, make_response, abort, redirect, url_for, request
+from wtforms import StringField, validators
+
+
+# instancias
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ssscetiro"
+
+
+HOROSCOPO = {
+    1: (("Rata", "rata.svg"), ("Compatibilidad1", "imagen.svg"), ("Compatibliidad2", "imagen.svg")),
+    2: (("Buey", "buey.svg"), ("Compatibilidad1", "imagen.svg"), ("Compatibilidad2", "imagen.svg")),
+    2: (("Caballo", "caballo.svg"), ("Compatibilidad1", "imagen.svg"), ("Compatibilidad2", "imagen.svg")),
+
+
+}
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.methods == "POST":
+        return "POST"
+    else:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run("127.0.0.1", 5000, debug=True)
---
+++ moduloTemplateFormularioHoroscopo.py
@@ -0,0 +1,11 @@
+from wtforms import StringField, validators
+
+
+class TemplateFormularioHoroscopo(Form):
+    anyo = StringField("Año:", validators=[
+        validators.data_required(
+            message="Estas obligado a rellenar este campo"),
+        validators.length(
+            min=4, max=5, message="Año no permitido"),
+        validators.optional(strip_whitespace=True)
+    ], description="Introduce Año:"
