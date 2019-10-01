8d2453656db95f3a35b2bdd750d2cceab8439482
jose <josloe@gmail.com>
2019-10-01 19:50:36 +0200

    eee

--- moduloTemplateFormularioHoroscopo.py
+++ moduloTemplateFormularioHoroscopo.py
@@ -17,5 +17,32 @@ class TemplateFormularioHoroscopo(Form):
     ], description="Introduce Año:")
 
 
-def calcularHoroscopo():
-    pass
+class HoroscopoChino():
+    ANIMALES_HOROSCOPO_CHINO = {
+
+        1: (("Rata", "rata.svg"), ("Dragon", "dragon.svg"), ("Mono", "mono.svg")),
+        2: (("Buey", "buey.svg"), ("Serpiente", "serpiente.svg"), ("Gallo", "gallo.svg")),
+        3: (("Tigre", "tigre.svg"), ("Caballo", "caballo.svg"), ("Perro", "perro.svg")),
+        4: (("Conejo", "conejo.svg"), ("Cerdo", "cerdo.svg"), ("Cabra", "cabra.svg")),
+        5: (("Dragon", "dragon.svg"), ("Rata", "rata.svg"), ("Mono", "mono.svg")),
+        6: (("Serpiente", "serpiente.svg"), ("Buey", "buey.svg"), ("Gallo", "gallo.svg")),
+        7: (("Caballo", "caballo.svg"), ("Tigre", "tigre.svg"), ("Perro", "perro.svg")),
+        8: (("Cabra", "cabra.svg"), ("Conejo", "conejo.svg"), ("Cerdo", "cerdo.svg")),
+        9: (("Mono", "mono.svg"), ("Rata", "rata.svg"), ("Dragon", "dragon.svg")),
+        10: (("Gallo", "gallo.svg"), ("Buey", "buey.svg"), ("Serpiente", "serpiente.svg")),
+        11: (("Perro", "perro.svg"), ("Tigre", "tigre.svg"), ("Caballo", "caballo.svg")),
+        12: (("Cerdo", "cerdo.svg"), ("Conejo", "conejo.svg"), ("Cabra", "cabra.svg"))
+
+    }
+
+    def __init__(self, anyo_usuario):
+        self.anyo_usuario = anyo_usuario
+
+    def calcularSigno(self):
+        try:
+            for i in range(0, 12):
+                if self.anyo_usuario % 12 == i:
+                    return self.ANIMALES_HOROSCOPO_CHINO[i]
+
+        except ValueError:
+            return "Error al introducir el año"
